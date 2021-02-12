78f02ceedfd7145253aedd795050d7baff863962
EC2 Default User <ec2-user@ip-172-31-5-102.us-east-2.compute.internal>
2021-02-12 12:03:12 +0000

    Dependencies and Fisrt Page

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
+var chalk = require ('chalk');
+var debug = require('debug')('app');
+var morgan = require ('morgan');
+
+
+
 var app = express();
 
 
+app.use(morgan('combined'));
 app.get('/', function (req, res){
     res.send('Hello from my lirary App');
 })
 
 app.listen(5000, function(){
-    console.log('listening on port 5000');
+    console.log(`listening on port ${chalk.green('5000')}`);
 })
